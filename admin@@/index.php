264eeea863e3109f92be6dc7434898b21e5ccb19
tungng <nguyenthanhtungdits@gmail.com>
2016-09-07 21:45:19 +0700

    change admin page login

--- admin@@/index.php
+++ admin@@/index.php
@@ -23,6 +23,7 @@
     <!-- Custom Fonts -->
     <link href="bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
+    <link href="css/style.css" rel="stylesheet" type="text/css">
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
@@ -74,24 +75,39 @@ if(isset($_POST["Sign"]))
             <div class="col-md-4 col-md-offset-4">
                 <div class="login-panel panel panel-default">
                     <div class="panel-heading">
-                        <h3 class="panel-title">ĐĂNG NHẬP</h3>
+                        <h3 class="panel-title text-uppercase">Đăng nhập hệ thống</h3>
                     </div>
                     <div class="panel-body">
                         <form role="form" method="post" action="">
                             <fieldset>
                                 <div class="form-group">
-                                    <input class="form-control" placeholder="E-mail" name="email" type="email" autofocus>
+							        <label for="name" class="control-label text-uppercase">Tên đăng nhập</label>
+                                    <input class="form-control" type="text" id="name" placeholder="Nhập tên" name="name" autofocus>
                                 </div>
                                 <div class="form-group">
-						<input class="form-control" placeholder="Password" name="password" type="password" value="">
+							        <label for="name" class="control-label text-uppercase">Mật khẩu</label>                                    
+						            <div class="input-group">
+                                        <input class="form-control" placeholder="Nhập mật khẩu" id="password" name="password" type="password" value="">
+                                        <label for="password" class="input-group-addon glyphicon glyphicon-eye-open"></label>
+                                    </div> 
                                 </div>
-                                <div class="checkbox">
-                                    <label>
-                                        <input name="remember" type="checkbox" value="Remember Me">Remember Me
-                                    </label>
+                                <div class="form-group">
+							        <label for="name" class="control-label text-uppercase">Ngôn ngữ</label>
+                                    <select class="form-control" id="language" name="language">
+                                        <option>Tiếng Việt</option>
+                                        <option>Tiếng Anh</option>
+                                    </select>
+                                </div>
+                                <div class="row">
+                                    <div class="checkbox col-sm-6">
+                                        <label>
+                                            <input name="remember" type="checkbox" value="Remember Me">Lưu mật khẩu
+                                        </label>                               
+                                    </div>   
+                                    <div class="col-sm-6 help-block text-right"><a href="">Quên mật khẩu?</a></div>                                        
                                 </div>
                                 <!-- Change this to a button or input when using this as a form -->
-                                <input class="btn btn-lg btn-primary btn-block" type="submit" name="Sign" value="Sign in"/>
+                                <input class="btn btn-lg btn-primary btn-block text-uppercase" type="submit" name="Sign" value="Đăng nhập"/>
                             </fieldset>
                         </form>
                     </div>
